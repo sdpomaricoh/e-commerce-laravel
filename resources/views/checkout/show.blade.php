a74c40c72ff3256aa3fa813a49c0bba2ef08b412
Sergio Pomárico <sergiodavid21@gmail.com>
2017-03-25 00:41:13 -0500

    añadir variables de producto a la vista de checkout

--- resources/views/checkout/show.blade.php
+++ resources/views/checkout/show.blade.php
@@ -6,17 +6,15 @@
 			<div class="price-details">
 				<h3>Price Details</h3>
 				<span>Total</span>
-				<span class="total1">6200.00</span>
-				<span>Discount</span>
-				<span class="total1">10%(Festival Offer)</span>
+				<span class="total1">$ {{$total}} USD</span>
 				<span>Delivery Charges</span>
-				<span class="total1">150.00</span>
+				<span class="total1">$ 15.00 USD</span>
 				<div class="clearfix"></div>
 			</div>
 			<hr class="featurette-divider">
 			<ul class="total_price">
 			   <li class="last_price"> <h4>TOTAL</h4></li>
-			   <li class="last_price"><span>6150.00</span></li>
+			   <li class="last_price"><span>$ {{$total+15}} USD</span></li>
 			   <div class="clearfix"> </div>
 			</ul>
 			<div class="clearfix"></div>
@@ -24,6 +22,7 @@
 		</div>
 		<div class="col-md-9 cart-items">
 			<h1>My Shopping Bag (2)</h1>
+			@foreach ($products as $product)
 			<div class="cart-header">
 				<div class="close"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></div>
 				<div class="cart-sec">
@@ -32,12 +31,12 @@
 					</div>
 				   <div class="cart-item-info">
 						<ul class="qty">
-							<li><p>Size : 9 US</p></li>
+							<li><p>Size : {{$product->size}}</p></li>
 							<li><p>Qty : 1</p></li>
-							<li><p>Price each : $190</p></li>
+							<li><p>Price each : {{$product->pricing}}</p></li>
 						</ul>
 						<div class="delivery">
-							 <p>Service Charges : Rs.190.00</p>
+							 <p>Service Charges : Rs. 15.00</p>
 							 <span>Delivered in 2-3 bussiness days</span>
 							 <div class="clearfix"></div>
 						</div>
@@ -45,27 +44,7 @@
 				   <div class="clearfix"></div>
 				</div>
 			</div>
-			<div class="cart-header">
-				<div class="close"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></div>
-				<div class="cart-sec">
-					<div class="cart-item cyc">
-						<img src="img/grid11.jpg" class="img-responsive" alt=""/>
-					</div>
-				   <div class="cart-item-info">
-						<ul class="qty">
-							<li><p>Size : 9 US</p></li>
-							<li><p>Qty : 1</p></li>
-							<li><p>Price each : $190</p></li>
-						</ul>
-						<div class="delivery">
-							 <p>Service Charges : Rs.190.00</p>
-							 <span>Delivered in 2-3 bussiness days</span>
-							 <div class="clearfix"></div>
-						</div>
-				   </div>
-				   <div class="clearfix"></div>
-				</div>
-			</div>
+			@endforeach
 		</div>
 	</div>
 </div>
